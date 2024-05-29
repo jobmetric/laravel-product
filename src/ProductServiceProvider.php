6b28c3cc877e048a0bcffdf4de8a71bbb35f87c6
majid mohammadian <majeedmohammadian@gmail.com>
2024-05-29 23:39:59 +0330

    add service provider

---
+++ src/ProductServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace JobMetric\Product;
+
+use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
+use JobMetric\PackageCore\PackageCore;
+use JobMetric\PackageCore\PackageCoreServiceProvider;
+
+class ProductServiceProvider extends PackageCoreServiceProvider
+{
+    /**
+     * @param PackageCore $package
+     *
+     * @return void
+     * @throws MigrationFolderNotFoundException
+     */
+    public function configuration(PackageCore $package): void
+    {
+        $package->name('laravel-product')
+            ->hasConfig()
+            ->hasMigration()
+            ->hasTranslation();
+    }
+}
