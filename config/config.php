83d1e00bf4396b0439dcbe6452333f161d6d00ca
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-31 21:47:31 +0330

    added config tables

--- config/config.php
+++ config/config.php
@@ -17,4 +17,20 @@ return [
 
     "cache_time" => env("PRODUCT_CACHE_TIME", 0),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Table Name
+    |--------------------------------------------------------------------------
+    |
+    | Table name in database
+    */
+
+    "tables" => [
+        'product_interface_physical' => 'product_interface_physicals',
+        'product_interface_service' => 'product_interface_services',
+        'product_interface_digital' => 'product_interface_digitals',
+        'product_interface_asset' => 'product_interface_assets',
+        'product' => 'products',
+    ],
+
 ];
